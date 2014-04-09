02c4f61ba9806ae2626ee2a7cd9d470bc1fb79f1
James Brumond <kbjr14@gmail.com>
2014-04-09 08:07:30 -0700

    corrected endpoint rendering

--- template/endpoint/base.js
+++ template/endpoint/base.js
@@ -5,14 +5,12 @@ var HttpError  = require('dagger.js/lib/http-meta').HttpError;
 var {{ endpointName.class }}Endpoint = module.exports = new Endpoint({
 
 	route: '/{{ endpointName.hyphen }}',
-
 	{{#each methods}}
 	//
-	// {{upper this}} /{{ endpointName.hyphen }}
+	// {{upper this}} /{{ ../endpointName.hyphen }}
 	//
 	"{{ this }}": function(req) {
 		(new HttpError(405, 'Endpoint not configured')).send(req);
 	}{{#unless @last}},{{/unless}}
 	{{/each}}
-
 });
